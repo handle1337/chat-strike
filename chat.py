f76bdda4cd6895a7dae60ae2af3a27aabce120b1
0xhornet <mewsiof@gmail.com>
2023-11-29 02:13:00 -0700

    Implement DearPyGUI

--- chat.py
+++ chat.py
@@ -1,10 +1,36 @@
 
-import conparser as cp
 import openai
 
+import dearpygui.dearpygui as dpg
+import conparser as cp
 
 
 openai.api_key = cp.config['SETTINGS']['openaiapikey']
+
+class DataValues():
+    running = False
+
+
+
+def init(sender, app_data, user_data):
+    
+    if DataValues.running == False:
+        dpg.configure_item("start_button", label="Stop")
+        dpg.set_value(user_data, "Running: True")
+        DataValues.running = True
+    elif DataValues.running == True:
+        dpg.configure_item("start_button", label="Start")
+        dpg.set_value(user_data, "Running: False")
+        DataValues.running = False
+
+
+def save_config():
+    cp.config['SETTINGS']['username'] = dpg.get_value("username")
+    cp.config['SETTINGS']['gameconlogpath'] = dpg.get_value("conlog")
+    cp.config['SETTINGS']['chatkey'] = dpg.get_value("chat_keybind")
+    with open(cp.CONFIG_FILE, 'w') as configfile:
+        cp.config.write(configfile)
+
 
 def openai_interact(user: str, message: str, content="You are a uwu egirl, limit responses to 120 characters"):
     message = f"I'm {user}, {message}"
@@ -16,27 +42,71 @@ def openai_interact(user: str, message: str, content="You are a uwu egirl, limit
     reply = chat.choices[0].message.content
     return reply
 
+
 def main():
+    logfile = None
     username = ""
     message = ""
-
     game = cp.detect_game()
     print(game)
 
-    with open(cp.CON_LOG_FILE_PATH, encoding='utf-8') as logfile:
-        logfile.seek(0, 2)  # Point cursor to the end of console.log to retrieve latest line
-        while True:
-            line = cp.rt_file_read(logfile)
-            if not line:
-                continue
-            print(line) # Print each new line
-            username, message = cp.parse_log(game, line)
-            
-            if username and message:
-                print(f"[DEBUG] {username}: {message}:")
-                # This way we prevent chat-gpt from talking to itself
-                if cp.BLACKLISTED_USERNAME != username: 
-                    cp.sim_key_presses(openai_interact(username, message))
+    
+
+    dpg.create_context()
+    dpg.create_viewport(title='Chat-Strike', width=600, height=300)
+
+    with dpg.window(label="Chat-Strike", width=600, height=300, tag="Chat-Strike"):
+        dpg.add_text(f"Detected game: {game}")
+        
+        dpg.add_input_text(hint="Blacklisted username", default_value=cp.BLACKLISTED_USERNAME, tag="username")
+        dpg.add_input_text(hint=".log file path", default_value=cp.CON_LOG_FILE_PATH, tag="conlog")
+        dpg.add_input_text(hint="Openapi key", default_value=openai.api_key, password=True, tag="openapi_key")
+        dpg.add_input_text(hint="Chat keybind", default_value=cp.CHAT_KEY, tag="chat_keybind")
+
+        dpg.add_button(label="Save", callback=save_config)
+        status_text = dpg.add_text("Running: False")
+
+        dpg.add_button(label="Start", callback=init, user_data=status_text, tag="start_button")
+
+
+    dpg.set_primary_window("Chat-Strike", True)
+    dpg.setup_dearpygui()
+    dpg.show_viewport()
+
+    if cp.config['SETTINGS']['gameconlogpath'] != None:
+        logfile = open(cp.CON_LOG_FILE_PATH, encoding='utf-8')
+        logfile.seek(0, 2)
+
+
+    while dpg.is_dearpygui_running():
+        dpg.render_dearpygui_frame()
+
+        if DataValues.running == True:
+            if logfile:
+                line = cp.rt_file_read(logfile)
+                
+                if not line:
+                    continue
+                print(line)
+                username, message = cp.parse_log(game, line)
+
+                if username and message:
+                    print(f"[DEBUG] {username}: {message}:")
+                    # This way we prevent chat-gpt from talking to itself
+                    if cp.BLACKLISTED_USERNAME != username: 
+                        cp.sim_key_presses(openai_interact(username, message))
+                else:
+                    continue
+    
+    if logfile:
+        logfile.close()
+    dpg.destroy_context()
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
